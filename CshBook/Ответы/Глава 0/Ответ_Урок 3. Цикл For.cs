ea8a4e683f1cf347efe2379342c36a7989ce5cdd
Anton <anton005go.too@gmail.com>
2026-03-18 10:19:37 +0300

    ответы 3-1 урок

--- CshBook/Ответы/Глава 0/Ответ_Урок 3. Цикл For.cs
+++ CshBook/Ответы/Глава 0/Ответ_Урок 3. Цикл For.cs
@@ -1,177 +1,151 @@
-namespace CshBook.Answers.Chapter0.Lesson03ForLoop;
-public class AnswerLesson03ForLoop
+namespace CshBook.Answers.Chapter0.Lesson03ForLoop
 {
-    public static void Main_()
+    internal static class AnswerLesson03ForLoop
     {
-
-    // Выведите на экран n раз фразу "Silence is golden". Число n вводит пользователь.
-    int n1 = int.Parse(Console.ReadLine());
-    
-    for (int i = 0; i < n1; i++)
-    {
-        Console.WriteLine("Silence is golden");
-    }
-
-
-
-    // Выведите на экран прямоугольник из нулей. Количество строк вводит пользователь, количество столбцов равно 5.
-    
-    int rows = int.Parse(Console.ReadLine());
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < 5; j++)
+        public static void Main_()
         {
-            Console.Write("0");
+            Console.WriteLine("Урок 3. Цикл for");
+            Console.WriteLine("=================");
+            Console.WriteLine();
+
+            {
+                Console.WriteLine("1. Приветствие по расписанию");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("You are welcome!");
+                }
+                Console.WriteLine();
+            }
+
+            {
+                int n = 4;
+
+                Console.WriteLine("2. Повтор фразы");
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("Silence is golden");
+                }
+                Console.WriteLine();
+            }
+
+            {
+                int n = 5;
+
+                Console.WriteLine("3. Прямой и обратный счет");
+                Console.Write("От 1 до n: ");
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.Write($"{i} ");
+                }
+                Console.WriteLine();
+
+                Console.Write("От n до 1: ");
+                for (int i = n; i >= 1; i--)
+                {
+                    Console.Write($"{i} ");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+            {
+                int n = 7;
+                int sum = 0;
+
+                for (int i = 1; i <= n; i++)
+                {
+                    sum += i;
+                }
+
+                Console.WriteLine("4. Сумма диапазона");
+                Console.WriteLine($"Сумма чисел от 1 до {n} = {sum}");
+                Console.WriteLine();
+            }
+
+            {
+                Console.WriteLine("5. Только нужные числа");
+                for (int number = 1; number <= 100; number++)
+                {
+                    if (number % 3 == 0 || number % 5 == 0)
+                    {
+                        Console.Write($"{number} ");
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+            {
+                int n = 7;
+
+                Console.WriteLine("6. Таблица для одного числа");
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine($"{n} * {i} = {n * i}");
+                }
+                Console.WriteLine();
+            }
+
+            {
+                int n = 40;
+
+                Console.WriteLine("7. Степени двойки");
+                for (int value = 1; value <= n; value *= 2)
+                {
+                    Console.Write($"{value} ");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+            {
+                int height = 3;
+                int width = 6;
+
+                Console.WriteLine("8. Прямоугольник из символов");
+                for (int row = 0; row < height; row++)
+                {
+                    for (int column = 0; column < width; column++)
+                    {
+                        Console.Write("#");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+
+            {
+                int n = 5;
+
+                Console.WriteLine("9. Рамка");
+                for (int row = 0; row < n; row++)
+                {
+                    for (int column = 0; column < n; column++)
+                    {
+                        bool isBorder = row == 0 || row == n - 1 || column == 0 || column == n - 1;
+                        Console.Write(isBorder ? "*" : " ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+
+            {
+                int rows = 3;
+                int seatsPerRow = 5;
+
+                Console.WriteLine("10. Ряды в кинотеатре");
+                for (int row = 1; row <= rows; row++)
+                {
+                    Console.Write($"Ряд {row}: ");
+                    for (int seat = 1; seat <= seatsPerRow; seat++)
+                    {
+                        Console.Write($"{seat} ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
         }
-        Console.WriteLine();
-    }
-    
-    
-    
-    // Вывести на экран фигуру из звездочек:
-    // *******
-    // *******
-    // *******
-    // *******
-    // (квадрат из n строк, в каждой строке n звездочек)
-    
-    int n2 = int.Parse(Console.ReadLine());
-    
-    for (int i = 0; i < n2; i++)
-    {
-        for (int j = 0; j < n2; j++)
-        {
-            Console.Write("*");
-        }
-    
-        Console.WriteLine();
-    }
-    
-    
-    // Вывести на экран числа от 1000 до 9999 такие, что среди цифр есть цифра 3.
-
-
-
-    for (int i = 1000; i < 10000; i++)
-    {
-        int a1 = i / 1000;
-        int a2 = i / 100 % 10;
-        int a3 = i % 100 / 10;
-        int a4 = i % 10;
-    
-    
-        if (a1 == 3 ||
-            a2 == 3 ||
-            a3 == 3 ||
-            a4 == 3)
-        {
-            Console.WriteLine(i);
-        }
-        
-    }
-    
-    
-    // Вывести на экран:
-    // AAABBBAAABBBAAABBB
-    // BBBAAABBBAAABBBAAA
-    // AAABBBAAABBBAAABBB
-    // (таких строк n, в каждой строке m троек AAA)
-    
-    int n3 = int.Parse(Console.ReadLine());
-    int m3 = int.Parse(Console.ReadLine());
-    
-    for (int i = 0; i < n3; i++)
-    {
-        string pattern = i % 2 == 0 ? "AAABBB" : "BBBAAA";
-    
-        for (int j = 0; j < m3; j++)
-        {
-            Console.Write(pattern);
-        }
-    
-        Console.WriteLine();
-    }
-    
-    
-    
-    // Вывести на экран:
-    // AAAAAAAAAAAAAAAA
-    // ABBBBBBBBBBBBBBA
-    // ABBBBBBBBBBBBBBA
-    // ABBBBBBBBBBBBBBA
-    // AAAAAAAAAAAAAAAA
-    // (количество строк вводит пользователь, ширина прямоугольника в два раза больше высоты)
-
-    int strCoutn = int.Parse(Console.ReadLine());
-    int wh = strCoutn * 2;
-    
-    for (int i = 0; i < strCoutn; i++)
-    {
-        for (int j = 0; j < wh; j++)
-        {
-            string ch = "A";
-    
-            if (i != strCoutn - 1 &&
-                i != 0 &&
-                j != wh - 1 &&
-                j != 0)
-                ch = "B";
-    
-            Console.Write(ch);
-        }
-    
-        Console.WriteLine();
-    }
-
-
-    //
-    // Выведите на экран квадрат из нулей и единиц, причем нули находятся только на диагонали квадрата. Всего в квадрате сто цифр. Обе диагонали состоят из 0
-    //
-
-    int n4 = int.Parse(Console.ReadLine());
-    
-    for (int i = 0; i < n4; i++)
-    {
-        for (int j = 0; j < n4; j++)
-        {
-            string ch = "1";
-    
-            if (i == j ||
-                j + i == n4 - 1)
-                ch = "0";
-            
-            Console.Write(ch);
-        }
-    
-        Console.WriteLine();
-    }
-
-
-
-
-    // Вывести на экран числа от 1 до 100, которые делятся на 3 или на 5.
-    //
-    
-    for (int i = 1; i < 100; i++)
-    {
-        if (i % 3 == 0 && i % 5 == 0)
-            Console.WriteLine(i);
-    }
-    
-    
-
-    // Вывести на экран все числа от 1 до n, которые являются степенью двойки.
-    //  
-
-  
-    int n5 = int.Parse(Console.ReadLine());
-        
-    Console.WriteLine($"Степени двойки от 1 до {n5}:");
-        
-    for (int i = 1; i <= n5; i *= 2)
-    {
-        Console.WriteLine(i);
-    }
-
     }
 }
