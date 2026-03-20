c06667f9a313a29d70b2f14c83554ba620641181
Anton <anton005go.too@gmail.com>
2026-03-21 02:02:08 +0300

    ответы while

--- CshBook/Ответы/Глава 0/Ответ_Урок 4. Цикл While.cs
+++ CshBook/Ответы/Глава 0/Ответ_Урок 4. Цикл While.cs
@@ -1,106 +1,202 @@
-namespace CshBook.Answers.Chapter0.Lesson04WhileLoop;
-public class AnswerLesson04WhileLoop
+namespace CshBook.Answers.Chapter0.Lesson04WhileLoop
 {
-    public static void Main_()
+    public static class AnswerLesson04WhileLoop
     {
-        // 1. Напишите цикл while, который выведет числа от 1 до 10.
-
-        int i = 1;
-        while (i <= 10)
+        public static void Main_()
         {
-            Console.WriteLine(i);
-            i++;
+            Console.WriteLine("Урок 4. Цикл while");
+            Console.WriteLine("==================");
+            Console.WriteLine();
+
+            {
+                int number = 1;
+
+                Console.WriteLine("1. Числа от 1 до 10");
+                while (number <= 10)
+                {
+                    Console.Write($"{number} ");
+                    number++;
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+            {
+                int n = 7;
+                int current = 1;
+
+                Console.WriteLine("2. Числа от 1 до n");
+                while (current <= n)
+                {
+                    Console.Write($"{current} ");
+                    current++;
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+            {
+                int n = 6;
+                int current = 1;
+                int sum = 0;
+
+                while (current <= n)
+                {
+                    sum += current;
+                    current++;
+                }
+
+                Console.WriteLine("3. Сумма от 1 до n");
+                Console.WriteLine($"Сумма = {sum}");
+                Console.WriteLine();
+            }
+
+            {
+                int[] enteredNumbers = { 5, 8, 3, 0, 10 };
+                int index = 0;
+                int sum = 0;
+
+                while (index < enteredNumbers.Length)
+                {
+                    int current = enteredNumbers[index];
+                    if (current == 0)
+                    {
+                        break;
+                    }
+
+                    sum += current;
+                    index++;
+                }
+
+                Console.WriteLine("4. Сумма до сигнала остановки");
+                Console.WriteLine($"Сумма введенных чисел: {sum}");
+                Console.WriteLine();
+            }
+
+            {
+                int[] enteredNumbers = { 7, 4, 9, 12, 0, 6 };
+                int index = 0;
+                int evenCount = 0;
+
+                while (index < enteredNumbers.Length)
+                {
+                    int current = enteredNumbers[index];
+                    if (current == 0)
+                    {
+                        break;
+                    }
+
+                    if (current % 2 == 0)
+                    {
+                        evenCount++;
+                    }
+
+                    index++;
+                }
+
+                Console.WriteLine("5. Подсчет четных чисел");
+                Console.WriteLine($"Количество четных чисел: {evenCount}");
+                Console.WriteLine();
+            }
+
+            {
+                int number = 50724;
+                int digits = 0;
+
+                while (number > 0)
+                {
+                    digits++;
+                    number /= 10;
+                }
+
+                Console.WriteLine("6. Количество цифр");
+                Console.WriteLine($"Количество цифр: {digits}");
+                Console.WriteLine();
+            }
+
+            {
+                int target = 1000;
+                int[] replenishments = { 150, 200, 300, 400 };
+                int total = 0;
+                int count = 0;
+                int index = 0;
+
+                while (index < replenishments.Length && total < target)
+                {
+                    total += replenishments[index];
+                    count++;
+                    index++;
+                }
+
+                Console.WriteLine("7. Цель накоплений");
+                Console.WriteLine($"Пополнений понадобилось: {count}");
+                Console.WriteLine($"Накопленная сумма: {total}");
+                Console.WriteLine();
+            }
+
+            {
+                string[] attempts = { "qwerty", "0000", "12345" };
+                int index = 0;
+                string password;
+
+                Console.WriteLine("8. Проверка пароля");
+                do
+                {
+                    password = attempts[index];
+                    Console.WriteLine($"Попытка {index + 1}: {password}");
+                    index++;
+
+                    if (password != "12345")
+                    {
+                        Console.WriteLine("Пароль неверный");
+                    }
+                }
+                while (password != "12345" && index < attempts.Length);
+
+                Console.WriteLine("Доступ открыт");
+                Console.WriteLine();
+            }
+
+            {
+                int[] attempts = { -4, 0, 9 };
+                int index = 0;
+                int number;
+
+                Console.WriteLine("9. Проверка положительного числа");
+                do
+                {
+                    number = attempts[index];
+                    Console.WriteLine($"Введено: {number}");
+                    index++;
+                }
+                while (number <= 0 && index < attempts.Length);
+
+                Console.WriteLine("Число принято");
+                Console.WriteLine();
+            }
+
+            {
+                string[] answers = { "да", "да", "нет" };
+                int index = 0;
+                string answer;
+
+                Console.WriteLine("10. Мини-анкета");
+                do
+                {
+                    answer = answers[index];
+                    Console.WriteLine($"Ответ пользователя: {answer}");
+                    index++;
+
+                    if (answer == "да")
+                    {
+                        Console.WriteLine("Заполняем следующий блок анкеты...");
+                    }
+                }
+                while (answer == "да" && index < answers.Length);
+
+                Console.WriteLine("Анкета завершена");
+                Console.WriteLine();
+            }
         }
-        
-        // 2. Создайте цикл while, который будет находить сумму чисел от 1 до 100.
-
-        int sum = 0;
-        int i1 = 0;
-        while (i1 !=100)
-        {
-            i1++;
-            sum += i1;
-        }
-        
-        Console.WriteLine(sum);
-
-        //
-        // 3. Используя цикл while, выведите все нечетные числа от 1 до 20.
-
-        int i2 = 1;
-        while (i2 <= 20)
-        {
-            if (i2 % 2 != 0)
-                Console.WriteLine(i2);
-            i2++;
-        }
-
-        // 4. Реализуйте бесконечный цикл while, который выводит числа от 1 до бесконечности, но прерывается при достижении числа 15.
-        int i3 = 1;
-        while (true)
-        {
-            if (i3 == 15)
-                break;
-            Console.WriteLine(i3);
-            i3++;
-        }
-        
-        // 5. Напишите цикл while, который будет запрашивать у пользователя ввод чисел и завершится, когда пользователь введет число 0.
-
-        while (true)
-        {
-            int res = int.Parse(Console.ReadLine());
-            
-            if (res == 0)
-                break;
-        }
-        
-        Console.WriteLine("Конец");
-        
-        //     
-        // 1. Напишите цикл do while, который выведет "Hello, World!" 5 раз.
-
-        int i4 = 0;
-        do
-        {
-            Console.WriteLine("Hello, World!");
-            i4++;
-        } while (i4 <= 4);
-        
-
-        // 2. Напишите программу, которая запрашивает ввод числа у пользователя и проверяет, положительное оно или отрицательное, до тех пор, пока пользователь не введет число 0.
-        do
-        {
-            int ans = int.Parse(Console.ReadLine());
-            
-            if (ans == 0)
-                break;
-            else if (ans > 0)
-                Console.WriteLine("+");
-            else
-                Console.WriteLine("-");
-            
-        } while (true);
-
-
-        // 3. Напишите программу, которая запрашивает у пользователя пароль через цикл do while до тех пор, пока пароль не будет равен "12345".
-        string pass;
-        do
-        {
-            pass = Console.ReadLine();
-        
-        } while (pass != "12345");
-
-
-        // 4. Используя do while, создайте цикл, который на каждой итерации уменьшает число на 3, начиная с 30, и завершится, когда число станет меньше или равно 0.
-
-        int num = 30;
-        do
-        {
-            Console.WriteLine(num);
-            num -= 3;
-        } while (num > 0);
-
-
     }
 }
